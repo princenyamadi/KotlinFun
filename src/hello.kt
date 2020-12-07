bfab8c117389f008a0ae28422f0ea4ff5fbfdde0
princenyamadi <princenyamadi.pn@gmail.com>
2020-12-07 16:27:35 +0000

    Classes

--- src/hello.kt
+++ src/hello.kt
@@ -1,34 +1,13 @@
 fun main(args: Array<String>) {
-// Classes
-    class Dog(var name: String, var age: Int){
+//Nullables
+    var age: Int? = 28
 
+    age = 45
+    age  = null
 
+    var newNumber = age!! //turning a nullable to none null
 
-}
-
-    var myDawg = Dog("Dogie",8)
-    myDawg.name
-    myDawg.age
-
-    class Cat{
-        var name: String
-        var age: Int
-//
-//        init {
-//            name = ""
-//            age=0
-//
-//        }
-        constructor(name:String, age: Int){
-        this.age =age
-        this.name = name
-}
-
-        fun catInfo(){
-            println("$name is $age years")
-
-        }
-
-
+    if(age != null){
+        age!!
     }
 }
