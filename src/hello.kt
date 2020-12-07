1c982bfc4e9af9d33dcde26098709be3971e8efc
princenyamadi <princenyamadi.pn@gmail.com>
2020-12-07 14:59:10 +0000

    Variables, Strings and Ints

---
+++ src/hello.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+    println("Hello Kotlin")
+//    setting variable types variable_name: [type] = [value
+    var age: Int  = 28
+
+    println(age)
+
+     var favCandy: String = "snicker"
+
+
+//    String Interpolation
+    println("I am $age years old and I love $favCandy")
+
+    var name: String = "Prince"
+
+    println(name)
+
+}
